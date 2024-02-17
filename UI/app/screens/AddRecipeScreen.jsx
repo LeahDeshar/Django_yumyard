f69dc8ee88f2e7aaed6837a10a6675748d8def27
Leah Deshar <leah.desar@gmail.com>
2024-02-17 23:59:39 +0545

    added component

--- UI/app/screens/AddRecipeScreen.jsx
+++ UI/app/screens/AddRecipeScreen.jsx
@@ -1,11 +1,27 @@
-import { View, Text, Image, RefreshControl } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  RefreshControl,
+  TextInput,
+  PanResponder,
+  Animated,
+  TouchableOpacity,
+} from "react-native";
 import React, { useRef, useState } from "react";
 import Screen from "../components/Screen";
 import AppButton from "../components/AppButton";
 import { BottomSheetModal } from "@gorhom/bottom-sheet";
 import colors from "../config/colors";
-import { MaterialCommunityIcons, Entypo } from "@expo/vector-icons";
+import {
+  MaterialCommunityIcons,
+  Entypo,
+  MaterialIcons,
+} from "@expo/vector-icons";
 import ImageInput from "../components/ImageInput";
+import AppTextInput from "../components/AppTextInput";
+import { ListItemSeparator } from "../components/lists";
+import { StyleSheet } from "react-native";
 
 const AddRecipeScreen = () => {
   const BottomRef = useRef(null);
@@ -122,14 +138,53 @@ const RecipeAddBody = () => {
   };
   return (
     <View>
-      {/* <ImageInput
-        onImageChange={handleImageChange}
-        imageAsset={selectedImage}
-      /> */}
       <ImageInput
         onImageChange={handleImageChange}
         imageAsset={selectedImage}
       />
+      <View className={"px-5 bg-lightGray pb-5"}>
+        <View className={"pt-3"}>
+          <TextInput
+            placeholder="Enter Recipe Title"
+            className={"text-lg py-2 pl-2"}
+          />
+          <ListItemSeparator color={colors.opacity} />
+        </View>
+        <View className={"pt-3"}>
+          <TextInput
+            placeholder="Enter Recipe Description"
+            className={"text-lg py-2 pl-2"}
+            multiline={true}
+          />
+          <ListItemSeparator color={colors.opactity} />
+        </View>
+        <View className={"pt-3"}>
+          <View className={"flex-row justify-between items-center"}>
+            <Text className={"text-lg pt-2 pl-2"}>Serves</Text>
+            <View className="w-52">
+              <TextInput
+                placeholder="2 people"
+                className={"text-lg py-2 pl-2"}
+              />
+              <ListItemSeparator color={colors.opacity} />
+            </View>
+          </View>
+          <View className={"flex-row justify-between items-center"}>
+            <Text className={"text-lg py-2 pl-2"}>Cook Time</Text>
+            <View className="w-52">
+              <TextInput
+                placeholder="1hr 30 min"
+                className={"text-lg py-2 pl-2"}
+              />
+              <ListItemSeparator color={colors.opacity} />
+            </View>
+          </View>
+        </View>
+      </View>
+      <View
+        className={"mt-3 pt-3 px-5 bg-lightGray pb-5"}
+        style={{ height: 300 }}
+      ></View>
     </View>
   );
 };
