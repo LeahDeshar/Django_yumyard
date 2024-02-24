ad520d3276e297605d0a34b9ec4a68bdf89c07ac
Leah Deshar <leah.desar@gmail.com>
2024-02-24 10:09:58 +0545

    added the progress bar with the controls

--- UI/app/components/AppVideo.jsx
+++ UI/app/components/AppVideo.jsx
@@ -1,12 +1,29 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { View, StyleSheet, Button } from "react-native";
 import { ResizeMode, Video } from "expo-av";
 
 const AppVideo = () => {
   const [status, setStatus] = React.useState({});
-
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [progress, setProgress] = useState(0);
   const video = useRef(null);
+  const handlePlayPause = () => {
+    if (isPlaying) {
+      video.current.pauseAsync();
+    } else {
+      video.current.playAsync();
+    }
+    setIsPlaying(!isPlaying);
+  };
 
+  const handleStop = () => {
+    video.current.stopAsync();
+    setIsPlaying(false);
+  };
+
+  const handleProgress = (progress) => {
+    setProgress(progress.positionMillis / status.durationMillis);
+  };
   return (
     <View>
       <Video
@@ -14,25 +31,27 @@ const AppVideo = () => {
         style={styles.video}
         source={require("../assets/videos/1.mp4")}
         resizeMode={ResizeMode.COVER}
-        isLooping
         onError={(error) => console.log("Video playback error:", error)}
         shouldPlay
         onPlaybackStatusUpdate={(status) => {
-          setStatus(() => status);
+          setStatus(status);
+          handleProgress(status);
         }}
       />
-      <View style={styles.controls}>
-        <Button
-          title={isPlaying ? "Pause" : "Play"}
-          onPress={handlePlayPause}
-        />
-        <Button title="Stop" onPress={handleStop} />
+      <View className={"absolute  w-full justify-center items-center h-full"}>
+        <View style={styles.controls}>
+          <Button
+            title={isPlaying ? "Pause" : "Play"}
+            onPress={handlePlayPause}
+          />
+          <Button title="Stop" onPress={handleStop} />
+        </View>
+        <View style={styles.progressBarContainer}>
+          <View
+            style={[styles.progressBar, { width: `${progress * 100}%` }]}
+          ></View>
+        </View>
       </View>
-      <ProgressViewIOS
-        style={styles.progress}
-        progress={progress}
-        progressTintColor="#007AFF"
-      />
     </View>
   );
 };
@@ -41,6 +60,28 @@ const styles = StyleSheet.create({
   video: {
     width: "100%",
     height: 400,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  controls: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+  },
+  progressBarContainer: {
+    width: "90%",
+    height: 10,
+    backgroundColor: "#ccc",
+    marginTop: 10,
+  },
+  progressBar: {
+    height: "100%",
+    backgroundColor: "#007AFF",
   },
 });
 
