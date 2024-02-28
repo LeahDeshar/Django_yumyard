e65478cb5cd50feac0f1452170c70fbc5e559847
Leah Deshar <leah.desar@gmail.com>
2024-02-28 19:32:49 +0545

    search module

---
+++ UI/app/screens/HistoryList.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const HistoryList = () => {
+  return (
+    <View>
+      <Text>HistoryList</Text>
+    </View>
+  );
+};
+
+export default HistoryList;
---
+++ UI/app/screens/RecentScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const RecentScreen = () => {
+  return (
+    <View>
+      <Text>RecentScreen</Text>
+    </View>
+  );
+};
+
+export default RecentScreen;
